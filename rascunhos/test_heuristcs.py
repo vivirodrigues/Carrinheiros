01e828a472d84d0b27b819d1ca4017b94f0283ec
Maria Vitória Rodrigues Oliveira <mvitoriarodrigueso@gmail.com>
2021-02-24 15:40:06 -0300

    Delete rascunhos directory

--- rascunhos/test_heuristcs.py
+++
@@ -1,6 +0,0 @@
-from search import *
-from notebook import psource, heatmap, gaussian_kernel, show_map, final_path_colors, display_visual, plot_NQueens
-
-# Needed to hide warnings in the matplotlib sections
-import warnings
-warnings.filterwarnings("ignore")
